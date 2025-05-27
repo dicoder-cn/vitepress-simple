b48f36db9a317b3d2ce69f5d273f10f3584fe61d
1778871523@qq.com <1778871523@qq.com>
2025-05-27 10:09:52 +0800

    更新ESLint和Prettier配置，禁用操作符换行规则，增加最大行宽限制至200，优化vpconfig.ts文件的导入顺序和代码格式，提升代码可读性。

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -15,6 +15,9 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    'operator-linebreak': 'off', // 禁用操作符必须换行的规则
+    'max-len': ['error', { code: 200 }], // 增加最大行宽限制
+    'implicit-arrow-linebreak': 'off',
     // 确保 ESLint 使用单引号
     quotes: ['error', 'single'],
     // 禁用与 Prettier 冲突的规则
